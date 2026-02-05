77138352b81943aeb78f8c0c5b4b5e74f1512af9
Pulkit Jain <zenpulcit06@gmail.com>
2026-02-05 16:40:08 +0530

    angular frequency analysis

--- DSHM_analysis.py
+++ DSHM_analysis.py
@@ -9,8 +9,14 @@ v = data["v"]
 a = data["a"]
 E = data["E"]
 b = data["b"]
-k = data["k"]
-m = data["m"]
+k = data["k"].iloc[0]
+m = data["m"].iloc[0]
+
+E0 = E.iloc[0]
+mask = (E>0)&(E>0.05*E0)
+
+t_fit = t[mask]
+E_fit = E[mask]
 
 plt.plot(t, x)
 plt.title("x vs t")
@@ -39,5 +45,23 @@ plt.xlabel("Position(m)")
 plt.ylabel("Velocity(m/s)")
 plt.grid()
 plt.show()
+
+slope , intercept = np.polyfit(t_fit,np.log(E_fit),1)
+d_const = -slope/2
+
+print(f"damping constant = {d_const:.2f}")
+
+d_coff = 2*m*d_const
+print(f"damping cofficiant = {d_coff:.2f}")
+
+angular_f_ana = np.sqrt((k/m) - (d_coff/2*m)**2)
+
+angular_f_th = np.sqrt((k/m) - (b.iloc[0]/2*m)**2)
+
+
+print(f"Theoretical omega = {angular_f_th}")
+print(f"Analytical omega = {angular_f_ana}")
+
+
 
 
